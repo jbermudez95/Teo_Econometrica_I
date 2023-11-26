2032f58ad894a68c48b722a7748b6c04b8fce18e
Jose Carlo Bermúdez <bermudezjosecarlo@gmail.com>
2023-11-26 02:16:41 -0300

    Update jacobiano.m

--- Tarea2/jacobiano.m
+++ Tarea2/jacobiano.m
@@ -1,5 +1,5 @@
 function [jacobian] = jacobiano(beta,x,y)
-% hessiano.m: Esta función recibe como input el vector de betas, el vector
+% jacobiano.m: Esta función recibe como input el vector de betas, el vector
 % de la i-esima fila de la matriz de variables independientes "x_i", y el 
 % i-ésimo elemento de la variable dependiente "y_i". Evalua la CPO de la 
 % log-likelihood y entrega el Jacobiano.
